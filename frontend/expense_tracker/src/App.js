5b1b910cd19a4338c1b9af8976f4d57ac5ff645b
Dhruv-Mehta13 <144453468+Dhruv-Mehta13@users.noreply.github.com>
2024-05-01 00:02:44 +0530

    React_Initialize
    React_Initialize

---
+++ frontend/expense_tracker/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-100 p-4">
+      <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+      <p className="text-red-600">
+        This is a React app styled with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
